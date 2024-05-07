b8ca23f1471de276514720d6e6133bb0366b5eb6
NathanalexK <miarynathan@hotmail.com>
2024-05-07 03:25:31 -0700

    sprint01

---
+++ build.bat
@@ -0,0 +1,14 @@
+set project_name=summer
+
+set server_dir=D:\NATHANALEX\Tools\tomcat\webapps
+set classes_dir=D:\Toutes\ITU\S4\INF209 Web Dyn\Sprint\Summer\out\production\Summer
+set jar_dir=D:\Toutes\Java Library
+
+xcopy "%classes_dir%" "build\" /Y /E /I
+@REM xcopy "%web_dir%" "build\" /Y /E /I
+@REM copy "web\WEB-INF" "build\WEB-INF\"
+@REM xcopy "lib" "build\" /Y /E /I
+
+jar cf %project_name%.jar -C build .
+copy %project_name%.jar "%jar_dir%\%project_name%"
+@REM copy %project_name%.war "%server_dir%"
