f784baddbcc13d05bddd79b3206519fd370d7014
NathanalexK <miarynathan@hotmail.com>
2024-05-09 23:15:00 -0700

    Suppression webservlet

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/")
+
 public class FrontController extends HttpServlet {
 
     @Override
