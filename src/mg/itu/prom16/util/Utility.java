af6f0b7c8673518a864a5f511bb7c847009f29dd
NathanalexK <miarynathan@hotmail.com>
2024-06-21 09:04:42 -0700

    Object as method parameter

--- src/mg/itu/prom16/util/Utility.java
+++ src/mg/itu/prom16/util/Utility.java
@@ -1,6 +1,16 @@
 package mg.itu.prom16.util;
 
 public class Utility {
+    public static boolean isPrimitiveType(Class<?> type) {
+        switch (type.getSimpleName().toLowerCase()){
+            case "int", "integer", "double", "string", "localdate", "localtime", "localdatetime" -> {
+                return true;
+            }
+        }
+        return false;
+    }
 
-
+    public static String capitalize(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
 }
