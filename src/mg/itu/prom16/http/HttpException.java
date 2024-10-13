aac406a0bcc82235c28bebf1b7e0037cd7724a86
NathanalexK <miarynathan@hotmail.com>
2024-10-13 13:29:45 -0700

    Sprint11_2498

--- src/mg/itu/prom16/http/HttpException.java
+++ src/mg/itu/prom16/http/HttpException.java
@@ -1,5 +1,29 @@
 package mg.itu.prom16.http;
 
-public class HttpException extends Exception{
+public class HttpException extends Exception {
+    private Integer httpStatus;
+    private String message;
 
+    public HttpException(Integer httpStatus, String message) {
+        super(message);
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public Integer getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(Integer httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
